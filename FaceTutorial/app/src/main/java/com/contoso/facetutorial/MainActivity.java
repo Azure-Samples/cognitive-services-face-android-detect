a19c8d8688bb1c88184844f9234787ac17ae4dfd
John MacKenzie <v-johnma@microsoft.com>
2018-07-11 12:38:43 -0700

    Changed versionName to match client library version#. Added AlertDialog to display detect errors. Increased rectangle stroke width for better frame visibility. Shortened line lengths. General cleanup.

--- FaceTutorial/app/src/main/java/com/contoso/facetutorial/MainActivity.java
+++ FaceTutorial/app/src/main/java/com/contoso/facetutorial/MainActivity.java
@@ -1,8 +1,5 @@
 package com.contoso.facetutorial;
 
-import android.app.Activity;
-import android.os.Bundle;
-
 import java.io.*;
 import android.app.*;
 import android.content.*;
@@ -17,24 +14,27 @@ import com.microsoft.projectoxford.face.*;
 import com.microsoft.projectoxford.face.contract.*;
 
 public class MainActivity extends Activity {
+    private final String apiEndpoint = "<API endpoint>";
+    private final String subscriptionKey = "<Subscription Key>";
+
+    private final FaceServiceClient faceServiceClient =
+            new FaceServiceRestClient(apiEndpoint, subscriptionKey);
 
     private final int PICK_IMAGE = 1;
     private ProgressDialog detectionProgressDialog;
-
-    private FaceServiceClient faceServiceClient =
-            new FaceServiceRestClient("your API endpoint", "<Subscription Key>");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button1 = (Button)findViewById(R.id.button1);
+        Button button1 = findViewById(R.id.button1);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent gallIntent = new Intent(Intent.ACTION_GET_CONTENT);
-                gallIntent.setType("image/*");
-                startActivityForResult(Intent.createChooser(gallIntent, "Select Picture"), PICK_IMAGE);
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(
+                        intent, "Select Picture"), PICK_IMAGE);
             }
         });
 
@@ -44,32 +44,35 @@ public class MainActivity extends Activity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK &&
+                data != null && data.getData() != null) {
             Uri uri = data.getData();
             try {
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
-                ImageView imageView = (ImageView) findViewById(R.id.imageView1);
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(
+                        getContentResolver(), uri);
+                ImageView imageView = findViewById(R.id.imageView1);
                 imageView.setImageBitmap(bitmap);
 
-                // This is the new addition.
-                detectAndFrame(bitmap);
-
+                // Uncomment
+                //detectAndFrame(bitmap);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
-    // Detect faces by uploading face images
-    // Frame faces after detection
 
-    private void detectAndFrame(final Bitmap imageBitmap)
-    {
+    // Detect faces by uploading a face image.
+    // Frame faces after detection.
+    private void detectAndFrame(final Bitmap imageBitmap) {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
         ByteArrayInputStream inputStream =
                 new ByteArrayInputStream(outputStream.toByteArray());
+
         AsyncTask<InputStream, String, Face[]> detectTask =
                 new AsyncTask<InputStream, String, Face[]>() {
+                    String exceptionMessage = "";
+
                     @Override
                     protected Face[] doInBackground(InputStream... params) {
                         try {
@@ -78,56 +81,77 @@ public class MainActivity extends Activity {
                                     params[0],
                                     true,         // returnFaceId
                                     false,        // returnFaceLandmarks
-                                    null           // returnFaceAttributes: a string like "age, gender"
-                /* If you want value of FaceAttributes, try adding 4th argument like below.
-                            new FaceServiceClient.FaceAttributeType[] {
-                    FaceServiceClient.FaceAttributeType.Age,
-                    FaceServiceClient.FaceAttributeType.Gender }
-                */
+                                    null          // returnFaceAttributes:
+                                    /* new FaceServiceClient.FaceAttributeType[] {
+                                        FaceServiceClient.FaceAttributeType.Age,
+                                        FaceServiceClient.FaceAttributeType.Gender }
+                                    */
                             );
-                            if (result == null)
-                            {
-                                publishProgress("Detection Finished. Nothing detected");
+                            if (result == null){
+                                publishProgress(
+                                        "Detection Finished. Nothing detected");
                                 return null;
                             }
-                            publishProgress(
-                                    String.format("Detection Finished. %d face(s) detected",
-                                            result.length));
+                            publishProgress(String.format(
+                                    "Detection Finished. %d face(s) detected",
+                                    result.length));
                             return result;
                         } catch (Exception e) {
-                            publishProgress("Detection failed");
+                            exceptionMessage = String.format(
+                                    "Detection failed: %s", e.getMessage());
                             return null;
                         }
                     }
+
                     @Override
                     protected void onPreExecute() {
+                        //TODO: show progress dialog
                         detectionProgressDialog.show();
                     }
                     @Override
                     protected void onProgressUpdate(String... progress) {
+                        //TODO: update progress
                         detectionProgressDialog.setMessage(progress[0]);
                     }
                     @Override
                     protected void onPostExecute(Face[] result) {
+                        //TODO: update face frames
                         detectionProgressDialog.dismiss();
+
+                        if(!exceptionMessage.equals("")){
+                            showError(exceptionMessage);
+                        }
                         if (result == null) return;
-                        ImageView imageView = (ImageView)findViewById(R.id.imageView1);
-                        imageView.setImageBitmap(drawFaceRectanglesOnBitmap(imageBitmap, result));
+
+                        ImageView imageView = findViewById(R.id.imageView1);
+                        imageView.setImageBitmap(
+                                drawFaceRectanglesOnBitmap(imageBitmap, result));
                         imageBitmap.recycle();
                     }
-        };
+                };
+
         detectTask.execute(inputStream);
     }
 
-    private static Bitmap drawFaceRectanglesOnBitmap(Bitmap originalBitmap, Face[] faces) {
+    private void showError(String message) {
+        new AlertDialog.Builder(this)
+                .setTitle("Error")
+                .setMessage(message)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                    }})
+                .create().show();
+    }
+
+    private static Bitmap drawFaceRectanglesOnBitmap(
+            Bitmap originalBitmap, Face[] faces) {
         Bitmap bitmap = originalBitmap.copy(Bitmap.Config.ARGB_8888, true);
         Canvas canvas = new Canvas(bitmap);
         Paint paint = new Paint();
         paint.setAntiAlias(true);
         paint.setStyle(Paint.Style.STROKE);
         paint.setColor(Color.RED);
-        int stokeWidth = 2;
-        paint.setStrokeWidth(stokeWidth);
+        paint.setStrokeWidth(10);
         if (faces != null) {
             for (Face face : faces) {
                 FaceRectangle faceRectangle = face.faceRectangle;
