18fb1569f755f6b411d16bd9afcaa72abf9a1fe0
John MacKenzie <v-johnma@microsoft.com>
2018-07-11 08:03:20 -0700

    Add code from Steps 8 and 9 of FaceAPIinJavaForAndroidTutorial.

--- FaceTutorial/app/src/main/java/com/contoso/facetutorial/MainActivity.java
+++ FaceTutorial/app/src/main/java/com/contoso/facetutorial/MainActivity.java
@@ -3,11 +3,50 @@ package com.contoso.facetutorial;
 import android.app.Activity;
 import android.os.Bundle;
 
+import java.io.*;
+import android.app.*;
+import android.content.*;
+import android.net.*;
+import android.os.*;
+import android.view.*;
+import android.graphics.*;
+import android.widget.*;
+import android.provider.*;
+
 public class MainActivity extends Activity {
+
+    private final int PICK_IMAGE = 1;
+    private ProgressDialog detectionProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button1 = (Button)findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gallIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                gallIntent.setType("image/*");
+                startActivityForResult(Intent.createChooser(gallIntent, "Select Picture"), PICK_IMAGE);
+            }
+        });
+
+        detectionProgressDialog = new ProgressDialog(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri uri = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                ImageView imageView = (ImageView) findViewById(R.id.imageView1);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
